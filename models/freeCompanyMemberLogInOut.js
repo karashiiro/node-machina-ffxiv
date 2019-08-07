c0ca8348b719a93d7126734b4c19de1e63bac6d6
karashiiro <49822414+karashiiro@users.noreply.github.com>
2019-08-07 16:41:19 -0700

    revert

--- models/freeCompanyMemberLogInOut.js
+++ models/freeCompanyMemberLogInOut.js
@@ -3,7 +3,7 @@ const MachinaModels = require('./_MachinaModels.js');
 module.exports = (struct) => {
     struct.supertype = "freeCompanyMemberLogInOut";
     struct.fc = String.fromCodePoint(...struct.data.slice(0x1A, 0x48)).replace(/\0/g, "");
-    struct.character = String.fromCodePoint(...struct.data.slice(0x48)).replace(/\0/g, "");
+    struct.character = String.fromCodePoint(...struct.data.slice(0x68)).replace(/\0/g, "");
 
     let status = MachinaModels.getUint16(struct.data, 0x10);
     if (status === 0x0F) {
