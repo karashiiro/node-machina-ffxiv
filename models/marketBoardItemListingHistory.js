d25e688ae6a8577ae618a00f16e7365bc2e5118b
PERRIGAULT Alexandre <contact@alexandre-perrigault.fr>
2019-09-23 03:12:38 +0200

    Update marketBoardItemListingHistory.js
    Fix missing entries

--- models/marketBoardItemListingHistory.js
+++ models/marketBoardItemListingHistory.js
@@ -1,7 +1,6 @@
 const MachinaModels = require("./_MachinaModels.js");
-const MateriaHelper = require("./_MateriaHelper.js");
 
-const LISTING_LENGTH = 156;
+const LISTING_LENGTH = 52;
 
 module.exports = async (struct) => {
     struct.itemCatalogId = MachinaModels.getUint32(struct.data, 0x00);
@@ -10,17 +9,15 @@ module.exports = async (struct) => {
     struct.listings = [];
     for (let i = 0; i < 20; i++) {
         const salePrice = MachinaModels.getUint32(struct.data, 0x08 + (LISTING_LENGTH * i));
-        if (salePrice === 0) break;
 
         let itemListing = {};
-
         itemListing.salePrice = salePrice;
         itemListing.purchaseTime = MachinaModels.getUint32(struct.data, 0x0C + (LISTING_LENGTH * i));
         itemListing.quantity = MachinaModels.getUint32(struct.data, 0x10 + (LISTING_LENGTH * i));
-        itemListing.hq = struct.data[0x11 + (LISTING_LENGTH * i)] === 1 ? true : false;
-        itemListing.onMannequin = struct.data[0x13 + (LISTING_LENGTH * i)] === 1 ? true : false;
-        itemListing.buyerName = String.fromCodePoint(...struct.data.slice(0x14 + (LISTING_LENGTH * i), 0x35 + (LISTING_LENGTH * i))).replace(/\0/g, "");;
-        itemListing.itemCatalogId = MachinaModels.getUint16(struct.data, 0x38 + (LISTING_LENGTH * i));
+        itemListing.hq = struct.data[0x14 + (LISTING_LENGTH * i)] === 1 ? true : false;
+        itemListing.onMannequin = struct.data[0x16 + (LISTING_LENGTH * i)] === 1 ? true : false;
+        itemListing.buyerName = String.fromCodePoint(...struct.data.slice(0x17 + (LISTING_LENGTH * i), 0x38 + (LISTING_LENGTH * i))).replace(/\0/g, "");;
+        itemListing.itemCatalogId = MachinaModels.getUint32(struct.data, 0x39 + (LISTING_LENGTH * i));
 
         struct.listings.push(itemListing);
     }
