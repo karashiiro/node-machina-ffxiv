f78683918248f523eb8c6cf3c51a70791d3f8d94
PERRIGAULT Alexandre <contact@alexandre-perrigault.fr>
2019-09-24 19:13:48 +0200

    Update examineSearchInfo.js

--- models/examineSearchInfo.js
+++ models/examineSearchInfo.js
@@ -9,16 +9,13 @@ module.exports = (struct) => {
     struct.searchComment = String.fromCodePoint(...struct.data.slice(0x23, 0xE3)).replace(/\0/g, "");
     struct.fc = String.fromCodePoint(...struct.data.slice(0xE4, 0xFB)).replace(/\0/g, "");
 
-    struct.classJob = [];
+    struct.classJobs = [];
     for (let i = 0; i < CLASSJOB_TOTAL; i++) {
-        // https://github.com/SapphireServer/Sapphire/blob/develop/src/common/Network/PacketDef/Zone/ServerZoneDef.h#L138
-        // Here data are inverted
-        // DNC is missing
-        // first classJob is wrong !
-        struct.classJob.push({
-            level: MachinaModels.getUint16(struct.data, 0xFE + (i * 4)),
-            job: getJob(MachinaModels.getUint16(struct.data, 0x100 + (i * 4)))
-        });
+        const classJob = {
+            job: getJob(MachinaModels.getUint16(struct.data, 0x100 + (i * 4))),
+            level: MachinaModels.getUint16(struct.data, 0x102 + (i * 4))
+        };
+        struct.classJobs.push(classJob);
     }
 };
 
@@ -58,45 +55,42 @@ const getWorld = (id) => { // TODO: use World exd
     if (id ===  99) return "Sargatanas";
 };
 const getJob = (id) => {
-    if (id ===  1) return ""; // ? Unknow
-    if (id ===  2) return "GLD";
-    if (id ===  3) return "PGL";
-    if (id ===  4) return "MRD";
-    if (id ===  5) return "LNC";
-    if (id ===  6) return "ARC";
-    if (id ===  7) return "CNJ";
-    if (id ===  8) return "THM";
-    if (id ===  9) return "CRP";
-    if (id ===  10) return "BSM";
-    if (id ===  11) return "ARM";
-    if (id ===  12) return "GSM";
-    if (id ===  13) return "LTW";
-    if (id ===  14) return "WVR";
-    if (id ===  15) return "ALC";
-    if (id ===  16) return "CUL";
-    if (id ===  17) return "MIN";
-    if (id ===  18) return "BTN";
-    if (id ===  19) return "FSH";
-    if (id ===  20) return "PLD";
-    if (id ===  21) return "MNK";
-    if (id ===  22) return "WAR";
-    if (id ===  23) return "DRG";
-    if (id ===  24) return "BRD";
-    if (id ===  25) return "WHM";
-    if (id ===  26) return "BLM";
-    if (id ===  27) return "ACN";
-    if (id ===  28) return "SCH"; // Need confirm
-    if (id ===  29) return "SMN"; // Need confirm
-    if (id ===  30) return "ROG";
-    if (id ===  31) return "NIN";
-    if (id ===  32) return "MCH";
-    if (id ===  33) return "DRK";
-    if (id ===  34) return "AST";
-    if (id ===  35) return "SAM";
-    if (id ===  36) return "RDM";
-    if (id ===  37) return "BLU";
-    if (id ===  38) return "GNB";
-
-    // DNC is missing ?
+    if (id ===  1) return "GLD";
+    if (id ===  2) return "PGL";
+    if (id ===  3) return "MRD";
+    if (id ===  4) return "LNC";
+    if (id ===  5) return "ARC";
+    if (id ===  6) return "CNJ";
+    if (id ===  7) return "THM";
+    if (id ===  8) return "CRP";
+    if (id ===  9) return "BSM";
+    if (id ===  10) return "ARM";
+    if (id ===  11) return "GSM";
+    if (id ===  12) return "LTW";
+    if (id ===  13) return "WVR";
+    if (id ===  14) return "ALC";
+    if (id ===  15) return "CUL";
+    if (id ===  16) return "MIN";
+    if (id ===  17) return "BTN";
+    if (id ===  18) return "FSH";
+    if (id ===  19) return "PLD";
+    if (id ===  20) return "MNK";
+    if (id ===  21) return "WAR";
+    if (id ===  22) return "DRG";
+    if (id ===  23) return "BRD";
+    if (id ===  24) return "WHM";
+    if (id ===  25) return "BLM";
+    if (id ===  26) return "ACN";
+    if (id ===  27) return "SCH"; // Need confirm
+    if (id ===  28) return "SMN"; // Need confirm
+    if (id ===  29) return "ROG";
+    if (id ===  30) return "NIN";
+    if (id ===  31) return "MCH";
+    if (id ===  32) return "DRK";
+    if (id ===  33) return "AST";
+    if (id ===  34) return "SAM";
+    if (id ===  35) return "RDM";
+    if (id ===  36) return "BLU";
+    if (id ===  37) return "GNB";
+    if (id ===  38) return "DNC";
 };
-
