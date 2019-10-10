e3fa7b2835fa761051efef27751376ffe022cff6
karashiiro <49822414+karashiiro@users.noreply.github.com>
2019-10-10 14:07:45 -0700

    Add GP to updateHpMpTp, per Sapphire

--- models/default/updateHpMpTp.js
+++
@@ -1,9 +0,0 @@
-// This is an auto-generated file.
-
-const MachinaModels = require("../_MachinaModels.js")
-
-module.exports = async (struct) => {
-    struct.hp = MachinaModels.getUint32(struct.data, 0);
-    struct.mp = MachinaModels.getUint16(struct.data, 4);
-    struct.tp = MachinaModels.getUint16(struct.data, 6);
-};
---
+++ models/updateHpMpTp.js
@@ -0,0 +1,8 @@
+const MachinaModels = require("../_MachinaModels.js")
+
+module.exports = async (struct) => {
+    struct.hp = MachinaModels.getUint32(struct.data, 0x00);
+    struct.mp = MachinaModels.getUint16(struct.data, 0x04);
+    struct.tp = MachinaModels.getUint16(struct.data, 0x06);
+    struct.gp = MachinaModels.getUint16(struct.data, 0x08);
+};
