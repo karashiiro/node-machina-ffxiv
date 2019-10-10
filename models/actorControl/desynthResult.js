40421bbb79637cb344127599c7df4f1aba976ce7
Supamiu <contact@flavien-normand.fr>
2019-10-10 19:15:58 +0200

    Change desynth result packet to support HQ items

--- models/actorControl/desynthResult.js
+++ models/actorControl/desynthResult.js
@@ -7,6 +7,6 @@ module.exports = async (struct) => {
     
     struct.resultType = MachinaModels.getUint32(struct.data, 0x04);
     struct.itemID = MachinaModels.getUint16(struct.data, 0x0C);
-    struct.itemHQ = MachinaModels.getUint16(struct.data, 0x0E);
+    struct.itemHQ = MachinaModels.getUint16(struct.data, 0x0E) > 0;
     struct.exp = MachinaModels.getUint32(struct.data, 0x14);
 };
