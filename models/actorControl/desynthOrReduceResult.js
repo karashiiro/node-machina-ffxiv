04f94245d4fbf2635fe8ed51924be4489694e64d
Supamiu <contact@flavien-normand.fr>
2019-10-11 19:40:46 +0200

    Fix for desynth result's param3 value

--- models/actorControl/desynthOrReduceResult.js
+++ models/actorControl/desynthOrReduceResult.js
@@ -1,12 +1,13 @@
-const MachinaModels = require("../_MachinaModels.js")
+const MachinaModels = require("../_MachinaModels.js");
 
 module.exports = async (struct) => {
     delete struct.param1;
     delete struct.param3;
     delete struct.param5;
-    
+    const param3 = MachinaModels.getUint32(struct.data, 0x0C);
+
     struct.resultType = MachinaModels.getUint32(struct.data, 0x04);
-    struct.itemID = MachinaModels.getUint16(struct.data, 0x0C);
-    struct.itemHQ = MachinaModels.getUint16(struct.data, 0x0E) > 0;
+    struct.itemID = param3 % 100000;
+    struct.itemHQ = param3 > 100000;
     struct.exp = MachinaModels.getUint32(struct.data, 0x14);
 };
