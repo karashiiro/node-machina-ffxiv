7a7ddf547f87df0fc828d25a886e1b01e5270f3e
Supamiu <contact@flavien-normand.fr>
2019-10-12 11:42:09 +0200

    Small naming fix for CraftingUnk

--- models/actorControl/craftingUnk.js
+++ models/actorControl/craftingUnk.js
@@ -2,6 +2,6 @@ module.exports = async (struct) => {
     if (struct.param1 === 18 || struct.param1 === 19) {
         struct.itemID = struct.param2 % 1000000;
         struct.quantity = struct.param3;
-        struct.hq = struct.param2 > 1000000;
+        struct.itemHQ = struct.param2 > 1000000;
     }
 };
