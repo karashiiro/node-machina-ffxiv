d942510f459ff94ae198c629c28d862c6f84bd14
karashiiro <49822414+karashiiro@users.noreply.github.com>
2019-10-11 22:49:38 -0700

    True private class members

--- node-machina-ffxiv.js
+++ node-machina-ffxiv.js
@@ -10,188 +10,193 @@ require('./polyfill.js');
 
 const MachinaModels = require('./models/_MachinaModels.js');
 
-// Private module members (TODO: Make Symbol() objects)
-var _monitor;
-var _stdoutQueue;
-var _timeout;
-var _filter;
-
-var _monitorType;
-var _pid;
-var _ip;
-var _useSocketFilter;
-var _parseAlgorithm;
-var _noData;
-var _logger = () => {};
-
-var _args;
-var _exePath;
-
 // Public class
-class MachinaFFXIV extends EventEmitter {
-    constructor(options) {
-        super();
+const MachinaFFXIV = (() => {
+    const monitor = Symbol();
+    const stdoutQueue = Symbol();
+    const timeout = Symbol();
+    const filter = Symbol();
 
-        // Basic type checks.
-        if (options) {
-            if (options.monitorType && typeof options.monitorType != 'string') {
-                throw new TypeError("monitorType must be a string.");
-            } else if (options.monitorType) {
-                _monitorType = options.monitorType.replace(/[^a-zA-Z]/g, "");
-            }
+    const monitorType = Symbol();
+    const pid = Symbol();
+    const ip = Symbol();
+    const useSocketFilter = Symbol();
+    const parseAlgorithm = Symbol();
+    const noData = Symbol();
+    const logger = Symbol();
 
-            if (options.pid && typeof options.pid != 'number') {
-                throw new TypeError("PID must be a number.");
-            } else if (options.pid) {
-                _pid = Math.floor(options.pid);
-            }
+    const args = Symbol();
+    const exePath = Symbol();
 
-            if (options.ip && typeof options.ip != 'string') {
-                throw new TypeError("IP must be a string.");
-            } else if (options.ip) {
-                _ip = options.ip.replace(/[^0-9.]/g, "");
-            }
+    class MachinaFFXIV extends EventEmitter {
+        constructor(options) {
+            super();
 
-            if (options.useSocketFilter && typeof options.useSocketFilter != 'boolean') {
-                throw new TypeError("useSocketFilter must be a Boolean.");
-            } else if (options.useSocketFilter) {
-                _useSocketFilter = options.useSocketFilter;
-            }
+            this[logger] = () => {};
 
-            if (options.parseAlgorithm && typeof options.parseAlgorithm != 'string') {
-                throw new TypeError("parseAlgorithm must be a string.");
-            } else if (options.parseAlgorithm) {
-                _parseAlgorithm = options.parseAlgorithm.replace(/[^a-zA-Z]/g, "");
-                switch (_parseAlgorithm) {
-                    case "RAMHeavy": break;
-                    case "CPUHeavy": break;
-                    case "PacketSpecific": break;
-                    default: throw new Error("Invalid parsing algorithm provided!");
+            // Basic type checks.
+            if (options) {
+                if (options.monitorType && typeof options.monitorType != 'string') {
+                    throw new TypeError("monitorType must be a string.");
+                } else if (options.monitorType) {
+                    this[monitorType] = options.monitorType.replace(/[^a-zA-Z]/g, "");
+                }
+
+                if (options.pid && typeof options.pid != 'number') {
+                    throw new TypeError("PID must be a number.");
+                } else if (options.pid) {
+                    this[pid] = Math.floor(options.pid);
+                }
+
+                if (options.ip && typeof options.ip != 'string') {
+                    throw new TypeError("IP must be a string.");
+                } else if (options.ip) {
+                    this[ip] = options.ip.replace(/[^0-9.]/g, "");
+                }
+
+                if (options.useSocketFilter && typeof options.useSocketFilter != 'boolean') {
+                    throw new TypeError("useSocketFilter must be a Boolean.");
+                } else if (options.useSocketFilter) {
+                    this[useSocketFilter] = options.useSocketFilter;
+                }
+
+                if (options.parseAlgorithm && typeof options.parseAlgorithm != 'string') {
+                    throw new TypeError("parseAlgorithm must be a string.");
+                } else if (options.parseAlgorithm) {
+                    this[parseAlgorithm] = options.parseAlgorithm.replace(/[^a-zA-Z]/g, "");
+                    switch (this[parseAlgorithm]) {
+                        case "RAMHeavy": break;
+                        case "CPUHeavy": break;
+                        case "PacketSpecific": break;
+                        default: throw new Error("Invalid parsing algorithm provided!");
+                    }
+                }
+
+                if (options.noData && typeof options.noData != 'boolean') {
+                    throw new TypeError("noData must be a Boolean.");
+                } else if (options.noData) {
+                    this[noData] = options.noData;
+                }
+
+                if (options.logger && typeof options.logger != 'function') {
+                    throw new TypeError("logger must be a Function.");
+                } else if (options.logger) {
+                    this[logger] = options.logger;
                 }
             }
 
-            if (options.noData && typeof options.noData != 'boolean') {
-                throw new TypeError("noData must be a Boolean.");
-            } else if (options.noData) {
-                _noData = options.noData;
+            // Folders
+            const remoteDatapath = (options && options.remoteDataPath) || path.join(__dirname, './remote-data');
+            if (!fs.existsSync(remoteDatapath)) {
+                fs.mkdirSync(remoteDatapath);
             }
 
-            if (options.logger && typeof options.logger != 'function') {
-                throw new TypeError("logger must be a Function.");
-            } else if (options.logger) {
-                _logger = options.logger;
+            this[args] = [];
+            if (this[monitorType]) this[args].push(`--MonitorType ${this[monitorType]}`);
+            if (this[pid]) this[args].push(`--ProcessID ${this[pid]}`);
+            if (this[ip]) this[args].push(`--LocalIP ${this[ip]}`);
+            if (this[useSocketFilter]) this[args].push("--UseSocketFilter");
+            if (this[parseAlgorithm]) this[args].push(`--ParseAlgorithm ${this[parseAlgorithm]}`);
+            this[exePath] = (options && options.machinaExePath) || path.join(__dirname, '/MachinaWrapper/MachinaWrapper.exe');
+            if (!fs.existsSync(this[exePath])) {
+                throw new Error(`MachinaWrapper not found in ${this[exePath]}`);
             }
-        }
+            this[monitor] = spawn(this[exePath], this[args]);
+            this[logger](`[${getTime()}] MachinaWrapper spawned with arguments "${this[args].toString()}"`);
 
-        // Folders
-        const remoteDatapath = (options && options.remoteDataPath) || path.join(__dirname, './remote-data');
-        if (!fs.existsSync(remoteDatapath)) {
-            fs.mkdirSync(remoteDatapath);
-        }
+            MachinaModels.loadDefinitions(options && options.definitionsDir);
 
-        _args = [];
-        if (_monitorType) _args.push(`--MonitorType ${_monitorType}`);
-        if (_pid) _args.push(`--ProcessID ${_pid}`);
-        if (_ip) _args.push(`--LocalIP ${_ip}`);
-        if (_useSocketFilter) _args.push("--UseSocketFilter");
-        if (_parseAlgorithm) _args.push(`--ParseAlgorithm ${_parseAlgorithm}`);
-        _exePath = (options && options.machinaExePath) || path.join(__dirname, '/MachinaWrapper/MachinaWrapper.exe');
-        if (!fs.existsSync(_exePath)) {
-            throw new Error(`MachinaWrapper not found in ${_exePath}`);
-        }
-        _monitor = spawn(_exePath, _args);
-        _logger(`[${getTime()}] MachinaWrapper spawned with arguments "${_args.toString()}"`);
+            this[filter] = [];
 
-        MachinaModels.loadDefinitions(options && options.definitionsDir);
+            // Create events to route outputs.
+            this[stdoutQueue] = ""; // A queue so that we don't get too much or too little of the buffer at once.
 
-        _filter = [];
+            this[monitor].on('error', (err) => {
+                this[logger](err);
+            });
 
-        // Create events to route outputs.
-        _stdoutQueue = ""; // A queue so that we don't get too much or too little of the buffer at once.
+            // { type: raw
+            //   connection: ip1=>ip2
+            //   operation: send/receive
+            //   epoch
+            //   data }
+            readline.createInterface({
+                input: this[monitor].stdout,
+                terminal: false
+            }).on('line', (line) => {
+                // If the C# program hangs for whatever reason.
+                /*if (this[timeout]) {
+                    clearTimeout(this[timeout]);
+                    this[timeout] = setTimeout(this.reset, 1200000);
+                }*/
 
-        _monitor.on('error', (err) => {
-            _logger(err);
-        });
+                this[stdoutQueue] += line;
+                if (this[stdoutQueue].indexOf("}") !== -1) { // A full JSON.
+                    let content = JSON.parse(this[stdoutQueue].slice(0, this[stdoutQueue].indexOf("}") + 1));
 
-        // { type: raw
-        //   connection: ip1=>ip2
-        //   operation: send/receive
-        //   epoch
-        //   data }
-        readline.createInterface({
-            input: _monitor.stdout,
-            terminal: false
-        }).on('line', (line) => {
-            // If the C# program hangs for whatever reason.
-            /*if (_timeout) {
-                clearTimeout(_timeout);
-                _timeout = setTimeout(this.reset, 1200000);
-            }*/
+                    if (this[filter].length === 0 ||
+                            this[filter].includes(content.type) ||
+                            this[filter].includes(content.subType) |
+                             this[filter].includes(content.superType)) {
+                        content.data = new Uint8Array(content.data); // Why store bytes as 32-bit integers?
 
-            _stdoutQueue += line;
-            if (_stdoutQueue.indexOf("}") !== -1) { // A full JSON.
-                let content = JSON.parse(_stdoutQueue.slice(0, _stdoutQueue.indexOf("}") + 1));
+                        MachinaModels.parseAndEmit(this[logger], content, this[noData], this); // Parse packet data
+                        this.emit('raw', content); // Emit a catch-all event
+                    }
 
-                if (_filter.length === 0 ||
-                        _filter.includes(content.type) ||
-                        _filter.includes(content.subType) |
-                         _filter.includes(content.superType)) {
-                    content.data = new Uint8Array(content.data); // Why store bytes as 32-bit integers?
-
-                    MachinaModels.parseAndEmit(_logger, content, _noData, this); // Parse packet data
-                    this.emit('raw', content); // Emit a catch-all event
+                    this[stdoutQueue] = this[stdoutQueue].indexOf("{") === -1 // Clear the queue
+                        ? ""
+                        : this[stdoutQueue].slice(this[stdoutQueue].indexOf("{"), this[stdoutQueue].indexOf("{"));
                 }
+            });
 
-                _stdoutQueue = _stdoutQueue.indexOf("{") === -1 // Clear the queue
-                    ? ""
-                    : _stdoutQueue.slice(_stdoutQueue.indexOf("{"), _stdoutQueue.indexOf("{"));
-            }
-        });
+            this[monitor].stderr.on('data', (err) => {
+                this[logger](err);
+            });
 
-        _monitor.stderr.on('data', (err) => {
-            _logger(err);
-        });
+            this[monitor].once('close', (code) => {
+                this[logger](`[${getTime()}] MachinaWrapper closed with code: ${code}`);
+            });
+        }
 
-        _monitor.once('close', (code) => {
-            _logger(`[${getTime()}] MachinaWrapper closed with code: ${code}`);
-        });
-    }
+        async parse(struct) {
+            return await MachinaModels.parse(this[logger], struct, this[noData], this);
+        }
 
-    async parse(struct) {
-        return await MachinaModels.parse(_logger, struct, _noData, this);
-    }
+        filter(filter) {
+            if (!filter) return;
+            this[filter] = filter.slice(0);
+        }
 
-    filter(filter) {
-        if (!filter) return;
-        _filter = filter.slice(0);
-    }
+        reset(callback) {
+            if (!this[exePath] || !this[args]) throw "No instance to reset.";
+            this[monitor] = spawn(this[exePath], this[args]);
+            this.start(callback);
+            this[logger](`[${getTime()}] MachinaWrapper reset!`);
+        }
 
-    reset(callback) {
-        if (!_exePath || !_args) throw "No instance to reset.";
-        _monitor = spawn(_exePath, _args);
-        this.start(callback);
-        _logger(`[${getTime()}] MachinaWrapper reset!`);
-    }
+        start(callback) {
+            if (!this[monitor]) throw "MachinaWrapper is uninitialized.";
+            this[monitor].stdin.write("start\n", callback);
+            this[logger](`[${getTime()}] MachinaWrapper started!`);
+        }
 
-    start(callback) {
-        if (!_monitor) throw "MachinaWrapper is uninitialized.";
-        _monitor.stdin.write("start\n", callback);
-        _logger(`[${getTime()}] MachinaWrapper started!`);
-    }
+        stop(callback) {
+            if (!this[monitor]) throw "MachinaWrapper is uninitialized.";
+            this[monitor].stdin.write("stop\n", callback);
+            this[logger](`[${getTime()}] MachinaWrapper stopped!`);
+        }
 
-    stop(callback) {
-        if (!_monitor) throw "MachinaWrapper is uninitialized.";
-        _monitor.stdin.write("stop\n", callback);
-        _logger(`[${getTime()}] MachinaWrapper stopped!`);
-    }
+        kill(callback) {
+            if (!this[monitor]) throw "MachinaWrapper is uninitialized.";
+            this[monitor].stdin.end("kill\n", callback);
+            this[monitor] = undefined;
+            this[logger](`[${getTime()}] MachinaWrapper killed!`);
+        }
+    };
 
-    kill(callback) {
-        if (!_monitor) throw "MachinaWrapper is uninitialized.";
-        _monitor.stdin.end("kill\n", callback);
-        _monitor = undefined;
-        _logger(`[${getTime()}] MachinaWrapper killed!`);
-    }
-};
+    return MachinaFFXIV;
+})();
 
 const getTime = () => {
     const time = new Date();
