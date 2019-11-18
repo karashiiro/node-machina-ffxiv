adc52c708981c6f12b77580607711171c80293cf
Supamiu <contact@flavien-normand.fr>
2019-11-18 22:46:29 +0100

    Fix stupid syntax error

--- models/eventUnk0.js
+++ models/eventUnk0.js
@@ -1,7 +1,7 @@
 const MachinaModels = require('./_MachinaModels.js');
 
 module.exports = (struct) => {
-    struct.eventId = MachinaModels.getUint32(0x08);
+    struct.eventId = MachinaModels.getUint32(struct.data, 0x08);
     // Links to ActionTimeline entry
-    struct.actionTimeline = MachinaModels.getUint32(0x1C);
+    struct.actionTimeline = MachinaModels.getUint32(struct.data, 0x1C);
 };
