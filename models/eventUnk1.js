62e45ef89b5db8a191272e865aeb88eeddc8c2fd
Supamiu <contact@flavien-normand.fr>
2019-11-19 19:26:10 +0100

    Fix eventUnk1 struct definition

--- models/eventUnk1.js
+++ models/eventUnk1.js
@@ -1,5 +1,7 @@
 const MachinaModels = require('./_MachinaModels.js');
 
 module.exports = (struct) => {
-    struct.eventId = MachinaModels.getUint32(0x08);
+    struct.eventId = MachinaModels.getUint32(struct.data, 0x00);
+    // Links to ActionTimeline entry
+    struct.actionTimeline = MachinaModels.getUint32(struct.data, 0x08);
 };
