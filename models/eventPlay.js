199a7920b3a3fc680df0503b8533c2d245f2e6c1
Supamiu <contact@flavien-normand.fr>
2019-11-21 22:48:38 +0100

    Fix for eventPlay's param5

--- models/eventPlay.js
+++ models/eventPlay.js
@@ -1,9 +1,9 @@
 const MachinaModels = require("./_MachinaModels.js");
 
 module.exports = async (struct) => {
-    struct.actorId = MachinaModels.getUint64(struct.data, 0);
-    struct.eventId = MachinaModels.getUint32(struct.data, 8);
-    struct.scene = MachinaModels.getUint16(struct.data, 12);
-    struct.flags = MachinaModels.getUint32(struct.data, 14);
-    struct.param5 = MachinaModels.getUint32(struct.data, 39);
+    struct.actorId = MachinaModels.getUint64(struct.data, 0x00);
+    struct.eventId = MachinaModels.getUint32(struct.data, 0x08);
+    struct.scene = MachinaModels.getUint16(struct.data, 0x0C);
+    struct.flags = MachinaModels.getUint32(struct.data, 0x0E);
+    struct.param5 = MachinaModels.getUint32(struct.data, 0x1C);
 };
