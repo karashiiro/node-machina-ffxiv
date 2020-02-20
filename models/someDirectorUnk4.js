e94b9105b92eadedef98403df480579ddac66d03
Supamiu <contact@flavien-normand.fr>
2020-02-20 22:23:46 +0100

    Add SomeDirectorUnk4 param3 definition

--- models/someDirectorUnk4.js
+++ models/someDirectorUnk4.js
@@ -5,4 +5,5 @@ module.exports = (struct) => {
     struct.param1 = MachinaModels.getUint32(struct.data, 0x04);
     // Links to ActionTimeline entry
     struct.actionTimeline = MachinaModels.getUint32(struct.data, 0x08);
+    struct.param3 = MachinaModels.getUint32(struct.data, 0x0C);
 };
