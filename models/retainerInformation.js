16422f93965784c676afdc1da2fc70cc41034f3d
Cara <caraxian040@gmail.com>
2020-02-24 02:00:23 +1030

    Fix offset for retainerInformation.level

--- models/retainerInformation.js
+++ models/retainerInformation.js
@@ -4,7 +4,7 @@ module.exports = async (struct) => {
     struct.retainerID = MachinaModels.getUint32(struct.data, 0x08);
     struct.itemCount = struct.data[0x11];
     struct.gil = MachinaModels.getUint32(struct.data, 0x14);
-    struct.level = struct.data[0x18];
+    struct.level = struct.data[0x1B];
     struct.ventureID = MachinaModels.getUint32(struct.data, 0x20);
     struct.ventureComplete = MachinaModels.getUint32(struct.data, 0x24);
     struct.name = MachinaModels.getString(struct.data, 0x29, 32);
