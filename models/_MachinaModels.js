7f9f3ed7a7f1baa033e84aede816c102b3e90bee
Supamiu <contact@flavien-normand.fr>
2020-03-01 16:37:19 +0100

    Renaming and better struct for updateTemporaryInventorySlot

--- models/_MachinaModels.js
+++ models/_MachinaModels.js
@@ -59,7 +59,7 @@ this.eventPlay                     = require('./eventPlay.js');
 this.eventPlay4                    = require('./eventPlay4.js');
 this.eventPlay8                    = require('./eventPlay8.js');
 this.someDirectorUnk4              = require('./someDirectorUnk4.js');
-this.useMooch                      = require('./useMooch.js');
+this.updateTemporaryInventorySlot  = require('./updateTemporaryInventorySlot.js');
 
 // Actor control packets
 this.actionStart                   = require('./actorControl/actionStart.js');
