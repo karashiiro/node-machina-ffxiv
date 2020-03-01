63b801d727a71425567f279bbbbe2348b909939e
karashiiro <49822414+karashiiro@users.noreply.github.com>
2020-03-01 11:25:46 -0800

    Update effectResult.js

--- models/effectResult.js
+++ models/effectResult.js
@@ -1,16 +1,27 @@
-// This is an auto-generated file.
-
 const MachinaModels = require("./_MachinaModels.js");
 
+const STATUS_EFFECT_ENTRY_LENGTH = 0x10;
+
 module.exports = async (struct) => {
+	struct.globalSequence = MachinaModels.getUint32(struct.data, 0x00);
     struct.actorID = MachinaModels.getUint32(struct.data, 0x04);
     struct.currentHp = MachinaModels.getUint32(struct.data, 0x0C);
     struct.currentMp = MachinaModels.getUint16(struct.data, 0x10);
-    struct.currentTp = MachinaModels.getUint16(struct.data, 0x12);
-    // struct.maxHp = MachinaModels.getUint32(struct.data, 0x14);
-    // struct.maxMp = MachinaModels.getUint16(struct.data, 16);
-    // struct.maxSomething = MachinaModels.getUint16(struct.data, 18);
-    struct.effectIndex = MachinaModels.getUint16(struct.data, 0x40);
-    struct.effectID = MachinaModels.getUint16(struct.data, 0x1E);
-    struct.actorID1 = MachinaModels.getUint32(struct.data, 0x28);
+    struct.unknown4 = MachinaModels.getUint16(struct.data, 0x12);
+	struct.unknown1 = struct.data[0x14];
+	struct.classId = struct.data[0x15];
+	struct.shieldPercentage = struct.data[0x16];
+	struct.entryCount = struct.data[0x17];
+	struct.unknown2 = MachinaModels.getUint16(struct.data, 0x18);
+	struct.statusEntries = [];
+    for (let i = 0; i < 4; i++) {
+		struct.statusEntries.push({
+			index: struct.data[0x20 + i + (STATUS_EFFECT_ENTRY_LENGTH * i)],
+			unknown3: struct.data[0x21 + i + (STATUS_EFFECT_ENTRY_LENGTH * i)],
+			id: MachinaModels.getUint16(struct.data, 0x22 + i + (STATUS_EFFECT_ENTRY_LENGTH * i)),
+			param: MachinaModels.getUint16(struct.data, 0x24 + i + (STATUS_EFFECT_ENTRY_LENGTH * i)),
+			duration: MachinaModels.getFloat(struct.data, 0x26 + i + (STATUS_EFFECT_ENTRY_LENGTH * i)),
+			sourceActorID: MachinaModels.getUint32(struct.data, 0x30 + i + (STATUS_EFFECT_ENTRY_LENGTH * i)),
+		});
+	}
 };
