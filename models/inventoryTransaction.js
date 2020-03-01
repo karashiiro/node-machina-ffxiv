404d7905a1947eb5159308bf41c9ea02379251b4
Flavien Normand <contact@flavien-normand.fr>
2020-03-01 18:39:13 +0100

    Proper naming for InventoryTransaction (#32)
    * ci: version bump to v2.33.1
    
    * fix stupid mistake on property naming
    
    * ci: version bump to v2.33.2
    
    * Import new build results from MachinaWrapperJSON
    
    * Proper naming for InventoryTransaction
    
    * ci: version bump to v2.33.3
    
    * Update package.json
    
    * Update package-lock.json
    
    Co-authored-by: karashiiro <49822414+karashiiro@users.noreply.github.com>

--- models/inventoryTransaction.js
+++ models/inventoryTransaction.js
@@ -9,10 +9,13 @@ module.exports = (struct) => {
             - 0xEB: deleting an item
             - 0xED: sent when zoning, I don't know more.
      */
-    struct.type = MachinaModels.getUint16(struct.data, 0x04);
+    struct.flag = MachinaModels.getUint16(struct.data, 0x04);
+    struct.padding = MachinaModels.getUint16(struct.data, 0x06);
     struct.containerId = MachinaModels.getUint16(struct.data, 0x0C);
     struct.slot = MachinaModels.getUint16(struct.data, 0x10);
-    struct.unknown = MachinaModels.getUint16(struct.data, 0x12);
+    struct.padding2 = MachinaModels.getUint16(struct.data, 0x12);
     struct.quantity = MachinaModels.getUint32(struct.data, 0x14);
     struct.catalogId = MachinaModels.getUint32(struct.data, 0x18);
+    struct.someActorId = MachinaModels.getUint32(struct.data, 0x1C);
+    struct.targetStorageId = MachinaModels.getUint32(struct.data, 0x20);
 };
