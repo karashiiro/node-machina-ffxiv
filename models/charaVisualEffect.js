10a3feb0daf59f57a9fb849e8e37a4a486fcc653
karashiiro <49822414+karashiiro@users.noreply.github.com>
2020-03-15 07:03:51 -0700

    charaVisualEffect

---
+++ models/charaVisualEffect.js
@@ -0,0 +1,6 @@
+const MachinaModels = require("./_MachinaModels.js")
+
+module.exports = async (struct) => {
+    struct.ID = MachinaModels.getUint32(struct.data, 0x00);
+    struct.padding = MachinaModels.getUint32(struct.data, 0x04);
+};
