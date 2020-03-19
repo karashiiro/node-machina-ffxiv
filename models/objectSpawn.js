b508dbfed0ba3fcc024e515c76504d986d9c1cc3
Flavien Normand <contact@flavien-normand.fr>
2020-03-19 15:02:17 +0100

    Support for ObjectSpawn and NpcSpawn position fix (#35)
    * fix flag offset value for inventoryModifyHandler
    
    * support for ObjectSpawn and NpcSpawn position fix

---
+++ models/objectSpawn.js
@@ -0,0 +1,21 @@
+// This is an auto-generated file.
+
+const MachinaModels = require("./_MachinaModels.js");
+
+module.exports = async (struct) => {
+    struct.spawnIndex = struct.data[0x00];
+    struct.objKind = struct.data[0x01];
+    struct.state = struct.data[0x02];
+    struct.unknown3 = struct.data[0x03];
+    struct.objId = MachinaModels.getUint32(struct.data, 0x04);
+    struct.actorId = MachinaModels.getUint32(struct.data, 0x08);
+    struct.levelId = MachinaModels.getUint32(struct.data, 0x0C);
+    struct.unknown10 = MachinaModels.getUint32(struct.data, 0x10);
+    struct.someActorId14 = MachinaModels.getUint32(struct.data, 0x14);
+    struct.gimmickId = MachinaModels.getUint32(struct.data, 0x18);
+    struct.scale = MachinaModels.getFloat(struct.data, 0x2C);
+    struct.unknown20a = MachinaModels.getUint16(struct.data, 0x30);
+    struct.rotation = MachinaModels.getUint16(struct.data, 0x32);
+    struct.housingLink = MachinaModels.getUint32(struct.data, 0x3C);
+    struct.position = MachinaModels.Position3(struct.data, 0x40);
+};
