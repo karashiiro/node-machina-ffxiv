e2611b5c3b6f9dfce04292912a6647c87b088fb2
Supamiu <contact@flavien-normand.fr>
2020-09-10 10:43:44 +0200

    refactor of eventPlay packets parsing
    !!! This commit introduces breaking changes to anyone using these packets (prob only me tho)

--- models/eventPlay8.js
+++ models/eventPlay8.js
@@ -1,8 +1,5 @@
 const MachinaModels = require('./_MachinaModels.js');
 
 module.exports = (struct) => {
-    struct.eventId = MachinaModels.getUint32(struct.data, 0x08);
-    struct.param1 = MachinaModels.getUint32(struct.data, 0x04);
-    // Links to ActionTimeline entry
-    struct.actionTimeline = MachinaModels.getUint32(struct.data, 0x1C);
+    MachinaModels.parseEventPlay(struct);
 };
