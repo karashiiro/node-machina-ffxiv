34b77e7521e332288de611f6900d165e9fb4e353
Supamiu <contact@flavien-normand.fr>
2021-01-22 20:50:09 +0100

    Fix for reductionResult and desynthResult structs

--- models/resultDialog/reductionResult.js
+++ models/resultDialog/reductionResult.js
@@ -2,16 +2,16 @@ const MachinaModels = require("../_MachinaModels.js");
 
 module.exports = async (struct) => {
     struct.itemID = MachinaModels.getUint32(struct.data, 0x08) % 500000;
-    struct.unknown2 = MachinaModels.getUint32(struct.data, 0x12);
+    struct.unknown2 = MachinaModels.getUint32(struct.data, 0x0C);
     struct.result = [];
     for (let i = 0; i < 3; i++) {
         const resultOffset = i * 8;
-        const itemResultID = MachinaModels.getUint32(struct.data, 0x16 + resultOffset) % 1000000;
+        const itemResultID = MachinaModels.getUint32(struct.data, 0x10 + resultOffset) % 1000000;
         if (itemResultID > 0) {
             struct.result.push({
                 itemID: itemResultID,
-                itemHQ: MachinaModels.getUint32(struct.data, 0x16 + resultOffset) > 1000000,
-                itemQuantity: MachinaModels.getUint32(struct.data, 0x20 + resultOffset)
+                itemHQ: MachinaModels.getUint32(struct.data, 0x10 + resultOffset) > 1000000,
+                itemQuantity: MachinaModels.getUint32(struct.data, 0x14 + resultOffset)
             })
         }
 
